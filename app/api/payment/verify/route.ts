8d2c7e3bc83792945f6939641c8842c69eb45a98
Aniket Bansode <aniketbansode935@gmail.com>
2026-07-30 14:42:12 +0530

    email done

--- app/api/payment/verify/route.ts
+++ app/api/payment/verify/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import crypto from "crypto";
 import { connectDB } from "@/lib/mongodb";
 import Order from "@/models/Order";
+import User from "@/models/User";
+import { sendEmail } from "@/lib/sendEmail";
 
 export async function POST(req: Request) {
   try {
@@ -41,6 +43,7 @@ export async function POST(req: Request) {
 
     const order = await Order.findByIdAndUpdate(
   orderId,
+
   {
     paymentStatus: "Paid",
 
@@ -54,9 +57,11 @@ export async function POST(req: Request) {
 
     razorpaySignature: razorpay_signature,
   },
+  
   {
     new: true,
   }
+  
 );
     if (!order) {
   return NextResponse.json(
@@ -67,6 +72,40 @@ export async function POST(req: Request) {
     {
       status: 404,
     }
+  );
+}
+const user = await User.findById(order.userId);
+
+if (user?.email) {
+  await sendEmail(
+    user.email,
+    "Payment Successful ✅",
+    `
+      <div style="font-family:Arial,sans-serif;padding:20px">
+        <h2>🎮 Gaming Thumbnail Marketplace</h2>
+
+        <p>Hello <b>${user.name}</b>,</p>
+
+        <p>Your payment has been received successfully.</p>
+
+        <hr/>
+
+        <p><b>Template:</b> ${order.templateName}</p>
+        <p><b>Amount:</b> ₹${order.price}</p>
+        <p><b>Status:</b> Paid ✅</p>
+
+        <hr/>
+
+        <p>
+          Your order is now being processed.
+          You'll receive another email when your thumbnail is completed.
+        </p>
+
+        <br/>
+
+        <h3>Thank you for your purchase ❤️</h3>
+      </div>
+    `
   );
 }
 
